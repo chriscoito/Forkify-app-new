7eea08012b7b37200443fe7c062801dddd6c553b
chriscoito <christian.coito1@gmail.com>
2025-11-01 13:39:57 -0300

    Initial comit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10000;
+export const RES_PER_PAGE = 10;
+export const KEY = '3474de18-e6e5-41f3-b2fc-5d9a55baa649';
+export const MODAL_CLOSE_SEC = 200.5;
